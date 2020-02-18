7a6a9a283750e9673e7899be3840eaac169ab888
刘尊文 <youfengyiyang@163.com>
2020-02-18 08:33:25 +0000

    更新 alert.py

--- alert.py
+++ alert.py
@@ -3,6 +3,7 @@ import json
 
 access_token = 'd54fd978ea84b3e43654bdff63e63213d8ba24c70eaaee013cacfcb5071d37d8'
 project_name = '会议系统meeting'
+env = "测试环境"
 
 class SendAlert:
     
@@ -14,11 +15,11 @@ class SendAlert:
                 "title": "自动化测试",
                 "text": "#### API测试 \n" +
                     "> 项目名称: {}\n\n".format(project_name) +
+                    "> 应用环境: {}\n\n".format(env)
                     "> api路径: {}\n\n".format('xxxxx') +
                     "> 测试结果: {}\n\n".format('ok') + 
             }
         }
-        print(send_data)
         url = 'https://oapi.dingtalk.com/robot/send?access_token={}'.format(access_token)
         req = requests.post(url=url, data=json.dumps(send_data).encode('utf-8'), headers=headers)
         result = req.json()
