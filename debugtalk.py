45bbf79f5c2b74852734e2e437df9fe293ab2358
chencong <onionch@126.com>
2020-02-26 15:31:21 +0800

    add clean data

--- debugtalk.py
+++ debugtalk.py
@@ -1,24 +1,45 @@
 import os
 import random
 import alert
+import psycopg2
 def gen_name():
     val = random.randint(0x4e00, 0x9fbf)
-    return "TEST"+chr(val)+chr(val)
+    return "AUTOTEST_"+chr(val)+chr(val)
 
 
 def gen_contact():
     val = random.randint(0x4e00, 0x9fbf)
-    return "TEST"+chr(val)
+    return "AUTOTEST_"+chr(val)
 
 
 def gen_mobile():
-    prelist=["130","131","132","133","134","135","136","137","138","139","147","150","151","152","153","155","156","157","158","159","186","187","188"]
+    prelist=["190"]
     return random.choice(prelist)+"".join(random.choice("0123456789") for i in range(8))
 
 
 def hook_print(any):
     print(any)
-    # DataInteg.genHtml()
+
+def clean_data():
+    print("Clear Test Date")
+    db = os.environ["PG_DB"]
+    user = os.environ["PG_USER"]
+    passwd = os.environ["PG_PASSWORD"]
+    host = os.environ["PG_HOST"]
+    port = os.environ["PG_PORT"]
+ 
+    #创建连接对象
+    conn = psycopg2.connect(database=db, user=user, password=passwd, host=host, port=port)
+    cur = conn.cursor() #创建指针对象
+ 
+    #删除数据
+    cur.execute("DELETE FROM meetings WHERE name LIKE %s",('AUTOTEST_%',))
+    cur.execute("DELETE FROM organs WHERE name LIKE %s",('190%',))
+ 
+    # 关闭连接
+    conn.commit()
+    cur.close()
+    conn.close()
 
 if __name__ == '__main__':
-    gen_mobile()
+    hook_print("It's working.")
--- run.py
+++ run.py
@@ -2,12 +2,40 @@
 from httprunner.api import HttpRunner
 from httprunner.report import gen_html_report
 from alert import DataInteg
+import os
+import psycopg2
 
-runner = HttpRunner(failfast=False)
+def clean_data():
+    print("Clear Test Date")
+    db = os.environ["PG_DB"]
+    user = os.environ["PG_USER"]
+    passwd = os.environ["PG_PASSWORD"]
+    host = os.environ["PG_HOST"]
+    port = os.environ["PG_PORT"]
+ 
+    #创建连接对象
+    conn = psycopg2.connect(database=db, user=user, password=passwd, host=host, port=port)
+    cur = conn.cursor() #创建指针对象
+ 
+    #删除数据
+    cur.execute("DELETE FROM meetings WHERE name LIKE %s",('AUTOTEST_%',))
+    cur.execute("DELETE FROM organs WHERE name LIKE %s",('190%',))
+ 
+    # 关闭连接
+    conn.commit()
+    cur.close()
+    conn.close()
 
-#执行测试用例集
-summary = runner.run('./testsuites')
-#生成报告
-gen_html_report(summary)
-#推送钉钉消息
-DataInteg.genHtml(summary)
+if __name__ == '__main__':
+    print("Start....")
+    runner = HttpRunner(failfast=False)
+
+    #执行测试用例集
+    summary = runner.run('./testsuites')
+    #生成报告
+    gen_html_report(summary)
+    #清理测试数据
+    clean_data()
+    #推送钉钉消息
+    # DataInteg.genHtml(summary)
+    print("End....")
